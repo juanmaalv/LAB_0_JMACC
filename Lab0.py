aedc49c54d8aa16db561de3f05ef107f58c57434
juanmaalv <47435120+juanmaalv@users.noreply.github.com>
2020-02-01 17:05:48 -0600

    Add files via upload

--- Lab0.py
+++ Lab0.py
@@ -13,15 +13,18 @@ Created on Thu Jan 23 09:58:59 2020
 # -- Autor: Nombre de autor                                                               -- #
 # -- ------------------------------------------------------------------------------------ -- #
 
-# -- ------------------------------------------------------------- Importar con funciones -- #
+#%% -- ------------------------------------------------------------- Importar con funciones -- #
 import funciones as fn
 import visualizaciones as vs
-
+import numpy as np
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
 
 # -- --------------------------------------------------------- Descargar precios de OANDA -- #
 
-# token de OANDA
+#%% token de OANDA
 OA_Ak = '630eb7b990acb33732bc201e28bf0d80-52218bf769c37309bee9440932b0f008'
 OA_In = "EUR_USD"                  # Instrumento
 OA_Gn = "D"                       # Granularidad de velas
@@ -38,7 +41,7 @@ vs_grafica1.show()
 
 # -- ------------------------------------------------------------------- Conteno de velas -- #
 
-# multiplicador de precios
+#%% multiplicador de precios
 pip_mult = 10000
 
 # -- 0A.1: Hora
@@ -55,18 +58,69 @@ vs_grafica2 = vs.g_boxplot_varios(p0_data=df_pe[['co']], p1_norm=False)
 vs_grafica2.show()
 
 
-#%%# -- 01: Mes de la vela.
+#%%# -- 01: Mes de la vela. 
 
-df_pe['Mes'] = [df_pe['TimeStamp'][i].month for i in range(0, len(df_pe['TimeStamp']))]
+df_pe['mes'] = [df_pe['TimeStamp'][i].month for i in range(0, len(df_pe['TimeStamp']))]
 
-# -- 02: Sesion de la vela.
+#%% -- 02: Sesion de la vela.
+df_pe["sesion"] = np.zeros(len(df_pe['High']))
+
+for i in range(1, len(df_pe['High'])):
+    
+    if  df_pe["hora"] > 21 or df_pe["hora"] <8:
+        df_pe['sesion'][i] = "asia"
+        
+    elif  df_pe["hora"] == 0:
+        df_pe['sesion'][i] = "asia_europa"
+        
+    elif  df_pe["hora"] > 8 and df_pe["hora"] <13:
+        df_pe['sesion'][i] = "europa"
+        
+    elif  df_pe["hora"] > 12 and df_pe["hora"] <17:
+        df_pe['sesion'][i] = "europa_america"
+        
+    else:
+        df_pe['sentido_c'][i] = "america"
 
 
+#%% -- 03: Amplitud OC esperada de vela para cualquier dia de la semana (Dist de Freq).
+df_pe['oc'] = [pip_mult*(float(df_pe['Close'][i])-float(df_pe['Open'][i])) for i in range(0, len(df_pe['Close']))]
 
+#%% -- 04: Amplitud HL esperada de vela para cualquier dia de la semana (Dist de Freq).
+df_pe['hl'] = [pip_mult*(float(df_pe['High'][i])-float(df_pe['Low'][i])) for i in range(0, len(df_pe['High']))]
 
+#%% -- 05: Evolucion de velas consecutivas (1: Alcistas, 0: Bajistas).
+df_pe["sentido"] = np.where(df_pe["oc"] > 0, 1, 0)
 
-# -- 03: Amplitud OC esperada de vela para cualquier dia de la semana (Dist de Freq).
-# -- 04: Amplitud HL esperada de vela para cualquier dia de la semana (Dist de Freq).
-# -- 05: Evolucion de velas consecutivas (1: Alcistas, 0: Bajistas).
-# -- 06: Maxima evolucion esperada de velas consecutivas (Dist Acum de Freq).
-# -- 07: Calculo + Grafica autopropuesta.
+#%% -- 06: Maxima evolucion esperada de velas consecutivas (Dist Acum de Freq).
+df_pe["sentido_c"] = np.zeros(len(df_pe['High']))
+
+for i in range(1, len(df_pe['High'])):
+    
+    if  df_pe['sentido'][i] == df_pe['sentido'][i-1]:
+        df_pe['sentido_c'][i] = df_pe['sentido_c'][i-1]+1
+        
+    else:
+        df_pe['sentido_c'][i] = 0
+
+#%% -- 07 Volatilidad Móvil
+
+df_pe["volatilidad5"] = [df_pe.iloc[i-5:i,9].std() for i in range(0, len(df_pe['High']))]
+df_pe["volatilidad25"] = [df_pe.iloc[i-25:i,9].std() for i in range(0, len(df_pe['High']))]
+df_pe["volatilidad50"] = [df_pe.iloc[i-50:i,9].std() for i in range(0, len(df_pe['High']))]
+        
+
+#%% -- 08: Calculo + Grafica autopropuesta.
+
+fig = px.histogram(df_pe, x= "sentido_c",
+                   title='Histograma de repetición de tendencias',
+                   labels={'# de días'}, # can specify one label per df column
+                           )
+fig.update_layout(
+    xaxis_title_text='Días Consecutivos Con Misma Tendencia', # xaxis label
+    yaxis_title_text='Frecuencia', # yaxis label
+    )
+    
+    
+fig.show()
+
